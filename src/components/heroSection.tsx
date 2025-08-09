0f9e036060e648df0c1f935bcff4971aa7d9d51d
iamukn <n.u.kingsley@gmail.com>
2025-08-09 02:23:35 +0100

    added a download cv button to the landing page

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -19,7 +19,7 @@ export default function HeroSection ({name, description, skills, cvUrl}: BioData
             <h2 style={{ whiteSpace: "pre-line" }}>{description}</h2>
             <div className=" btn-container">
                 <button className="hero-btn view-my-work-btn" onClick={() => {navigate('/projects')}}>View My Work</button>
-                <a className="hero-btn mild-border cv-btn" href={cvUrl} target="_blank">Download CV</a>
+                <a className="hero-btn mild-border cv-btn" href={cvUrl} target="_blank">View CV</a>
                 <button className="hero-btn contact-me-btn mild-border" onClick={() => {navigate('/get-in-touch')}}>Contact Me</button>
             </div>
             <div className="skills-container">
