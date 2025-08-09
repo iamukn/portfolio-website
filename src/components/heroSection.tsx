04ded49d6f0b3322fb0d10606510431082751005
iamukn <n.u.kingsley@gmail.com>
2025-08-09 02:51:38 +0100

    fixed a button

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -12,14 +12,18 @@ export default function HeroSection ({name, description, skills, cvUrl}: BioData
 
     const navigate = useNavigate();
 
+
+    function handleClick(url: string){
+        window.open(url, "_blank", "noopener,noreferrer")
+    }
     return <section className="heroSection">
         <center>
             <p className="intro"> Hello, I'm </p>
             <h1>{name}</h1>
             <h2 style={{ whiteSpace: "pre-line" }}>{description}</h2>
-            <div className=" btn-container">
+            <div className="btn-container">
                 <button className="hero-btn view-my-work-btn" onClick={() => {navigate('/projects')}}>View My Work</button>
-                <a className="hero-btn mild-border cv-btn" href={cvUrl} target="_blank">View CV</a>
+                <button className="hero-btn view-my-work-btn" id="cv-btn" onClick={() => handleClick(cvUrl)}>View CV</button>
                 <button className="hero-btn contact-me-btn mild-border" onClick={() => {navigate('/get-in-touch')}}>Contact Me</button>
             </div>
             <div className="skills-container">
